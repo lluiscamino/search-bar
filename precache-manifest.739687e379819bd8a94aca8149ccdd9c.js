9000be03095c7a9825020033d31d3f1d5847a23a
Lluís Camino <caminoperez.lluis@gmail.com>
2020-01-19 00:02:18 +0100

    Updates

---
+++ precache-manifest.739687e379819bd8a94aca8149ccdd9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa51fc65e7a77ae78c139401718bfabd",
+    "url": "/search-bar/index.html"
+  },
+  {
+    "revision": "a3ed386024879541f56e",
+    "url": "/search-bar/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "345dd1cc5859edaf34ed",
+    "url": "/search-bar/static/js/2.f813828d.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/search-bar/static/js/2.f813828d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a3ed386024879541f56e",
+    "url": "/search-bar/static/js/main.0b01a87b.chunk.js"
+  },
+  {
+    "revision": "d78ec6b774ab2a9ad865",
+    "url": "/search-bar/static/js/runtime-main.d2f581f0.js"
+  }
+]);
